380c854fd7589d1aceb5a1945c55efa56c50a8af
lucasoneves <casluhc@gmail.com>
2021-04-13 13:48:53 -0300

    Adding Middleware

--- src/index.js
+++ src/index.js
@@ -5,12 +5,24 @@ import App from "./container/App";
 import reportWebVitals from "./reportWebVitals";
 
 // Provider inject the Store into react components
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 // import counterReducer from "./store/reducers/counter";
 import reducer from './store/reducers/offers';
 
-const store = createStore(reducer);
+// Create the Middleware
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[MIDDLEWARE] Dispatching', action)
+      const result = next(action);
+      console.log('[MIDDLEWARE] next state', store.getState())
+      return result;
+    }
+  }
+}
+
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
